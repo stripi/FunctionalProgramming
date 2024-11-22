0e4339790e5b4d72df40fddb0b38fa9adb319226
Agnes <133767448+agnes-vj@users.noreply.github.com>
2024-11-22 11:48:02 +0000

    update till Task5

--- FunctionalProgramming/Exercises001.cs
+++ FunctionalProgramming/Exercises001.cs
@@ -12,6 +12,13 @@ namespace FunctionalProgramming
         public static Func<int, int> AddOne = num => num + 1;
 
         public static Func<int, int> SquareIt = num => num * num;
-        
+
+        public static Action<int> AddTen = num => Console.WriteLine(num + 10);
+
+        public static Predicate<string> GrammarCheck = word => (word.StartsWith('A') && word.EndsWith('!'));
+
+        public static Func<string, string, int> SumIndices = (word1, word2) => word1.IndexOf('a') + word2.IndexOf('e');
+
+
     }
 }
--- FunctionalProgramming/Program.cs
+++ FunctionalProgramming/Program.cs
@@ -6,7 +6,21 @@
         {
             int square = Exercises001.SquareIt(2);
             Console.WriteLine($"SquareIt: {square}");
+            List<int> numbers = new List<int> { 4, 15, 55, 78, 12 };
+            numbers.ForEach(Exercises001.AddTen);
 
+            List<string> words = new List<string> { "Amazing!", "apple!", "Awesome!", "Artichoke" };
+            Predicate<string> checkWord = Exercises001.GrammarCheck;
+            words.ForEach(word => Console.WriteLine(checkWord(word)));
+
+            Console.WriteLine(" Using directly inside");
+
+            words.ForEach(word => Console.WriteLine(Exercises001.GrammarCheck(word)));
+
+            string word1 = "start";
+            string word2 = "pale";
+
+            Console.WriteLine("Index of \'a\' in \"start\" + Index of \'e\' in \"pale\" " + Exercises001.SumIndices(word1,word2));
 
         }
     }
